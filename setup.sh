a256f36b3e44c107de82f97074007792d6873fcc
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-12-06 05:39:02 +0000

    changes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-export SPARK_HOME= /home/vscode/.local/lib/python3.10/site-packages/pyspark/pandas/spark
-export PATH=$SPARK_HOME/bin:$HADOOP_HOME/bin:$PATH
+brew update && brew install azure-cli
